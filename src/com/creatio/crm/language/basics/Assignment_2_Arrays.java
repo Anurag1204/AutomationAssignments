4b2b9f542b819549d1bae21c0871fb20304bfda6
Anurag Singh <anurags655@gmail.com>
2024-12-24 20:26:02 +0530

    Added code for Assignment-2

---
+++ src/com/creatio/crm/language/basics/Assignment_2_Arrays.java
@@ -0,0 +1,63 @@
+package com.creatio.crm.language.basics;
+
+public class Assignment_2_Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] categories = {"Groceries","Electronics"};
+		String[] subcategories = {"Beverages","Snacks","Dairy Products","Produce"};
+		
+		String[][][] Supermarket = new String[2][4][5];
+		
+		Supermarket[0][0][0]= "Cola";
+		Supermarket[0][0][1]= "Orange Juice";
+		Supermarket[0][0][2]= "Lemonade";
+		Supermarket[0][0][3]= "Green Tea";
+		Supermarket[0][0][4]= "Black Coffee";
+		
+		Supermarket[0][1][0]= "Popcorn";
+		Supermarket[0][1][1]= "Trail Mix";
+		Supermarket[0][1][2]= "Beef Jerky";
+		Supermarket[0][1][3]= "Salted Nuts";
+		Supermarket[0][1][4]= "Rice Cakes";
+		
+		Supermarket[0][2][0]= "Cottage Cheese";
+		Supermarket[0][2][1]= "Heavy Cream";
+		Supermarket[0][2][2]= "Ice Cream";
+		Supermarket[0][2][3]= "Parmesan Cheese";
+		Supermarket[0][2][4]= "Eggnog";
+		
+		Supermarket[0][3][0]= "Apples";
+		Supermarket[0][3][1]= "Bananas";
+		Supermarket[0][3][2]= "Carrots";
+		Supermarket[0][3][3]= "Potatoes";
+		Supermarket[0][3][4]= "Onions";
+		
+		Supermarket[1][0][0]= "Cola";
+		Supermarket[1][0][1]= "Orange Juice";
+		Supermarket[1][0][2]= "Lemonade";
+		Supermarket[1][0][3]= "Green Tea";
+		Supermarket[1][0][4]= "Black Coffee";
+		
+		Supermarket[1][1][0]= "Popcorn";
+		Supermarket[1][1][1]= "Trail Mix";
+		Supermarket[1][1][2]= "Beef Jerky";
+		Supermarket[1][1][3]= "Salted Nuts";
+		Supermarket[1][1][4]= "Rice Cakes";
+		
+		Supermarket[1][2][0]= "Cottage Cheese";
+		Supermarket[1][2][1]= "Heavy Cream";
+		Supermarket[1][2][2]= "Ice Cream";
+		Supermarket[1][2][3]= "Parmesan Cheese";
+		Supermarket[1][2][4]= "Eggnog";
+		
+		Supermarket[1][2][0]= "Apples";
+		Supermarket[1][2][1]= "Bananas";
+		Supermarket[1][2][2]= "Carrots";
+		Supermarket[1][2][3]= "Potatoes";
+		Supermarket[1][2][4]= "Onions";
+		
+	}
+
+}
