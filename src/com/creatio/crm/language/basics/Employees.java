eed6c1906b8e1c6dd31a2ac4cb1fee28fef2c151
Anurag Singh <anurags655@gmail.com>
2024-12-24 19:49:19 +0530

    Added code for Arrays_Classes_Objects

---
+++ src/com/creatio/crm/language/basics/Employees.java
@@ -0,0 +1,15 @@
+package com.creatio.crm.language.basics;
+
+public class Employees {
+	
+	String Employee_names1= "Bharath";
+	String Employee_names2="ABC";
+	String Employee_names3 = "XYZ";
+	
+	int employee_id1= 1234;
+	int employee_id2= 1235;
+	int employee_id3= 1236;
+
+
+
+}
