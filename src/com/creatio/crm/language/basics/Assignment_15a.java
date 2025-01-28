b2b836aa721a29437e6faa3ae708ca0593144b5e
Anurag Singh <anurags655@gmail.com>
2025-01-28 11:08:01 +0530

    Updated Code

--- src/com/creatio/crm/language/basics/Assignment_15a.java
+++ src/com/creatio/crm/language/basics/Assignment_15a.java
@@ -8,22 +8,19 @@ public class Assignment_15a {
 		// Removed all the white spaces by using replace method
 		String FilteredInput = input.replace(" ", "");
 
-		String word = "";
-
 		for (int i = 0; i < FilteredInput.length(); i++) {
 			for (int j = i + 1; j < FilteredInput.length(); j++) {
 
 				char char1 = FilteredInput.charAt(i);
 				char char2 = FilteredInput.charAt(j);
-				
-				if(char1!= char2) {
+
+				if (char1 != char2) {
 					System.out.println(FilteredInput.valueOf(char1) + FilteredInput.valueOf(char2));
 					System.out.println(FilteredInput.valueOf(char2) + FilteredInput.valueOf(char1));
 				}
 
 			}
 		}
-		
 
 	}
 
