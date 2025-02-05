2ef4ca0e96b125532775e6a190b88e07768b3664
Anurag Singh <anurags655@gmail.com>
2025-02-05 12:53:35 +0530

    updated test case

--- src/com/creatio/crm/language/basics/StudentTest.java
+++ src/com/creatio/crm/language/basics/StudentTest.java
@@ -3,19 +3,16 @@ package com.creatio.crm.language.basics;
 public class StudentTest {
 
 	public static void main(String[] args) {
-		
+
 		StudentGrades obj = new StudentGrades();
 
-        obj.addNewStudentGrade("Anurag",'A');
-        obj.addNewStudentGrade("Bob",'B');
-        obj.addNewStudentGrade("Charlie",'C');
+		obj.addNewStudentGrade("Anurag", 'A');
+		obj.addNewStudentGrade("Bob", 'B');
+		obj.addNewStudentGrade("Charlie", 'C');
 
+		obj.removeStudentGrade("Anurag");
 
-        obj.removeStudentGrade("Anurag");
-
-        obj.viewStudentGrade("Bob");
-
-
+		obj.viewStudentGrade("Bob");
 
 	}
 
